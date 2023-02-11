07fc2fc4d45aa5816b45fb285417c56f7410d82c
Silverseed44 <97930685+Silverseed44@users.noreply.github.com>
2023-02-11 12:12:05 -0500

    Update

--- Multiplexer.v
+++ Multiplexer.v
@@ -25,10 +25,12 @@ BusMuxInMDR,
 BusMuxInInPort,
 CSignExtended,
 
-BusMuxOut
+BusMuxOut,
+
+EncodeOut
 
 );
-input [31:0] BusMuxInR0,
+input [31:0] 	 BusMuxInR0,
                 BusMuxInR1,
                 BusMuxInR2,
                 BusMuxInR3,
@@ -45,19 +47,48 @@ input [31:0] BusMuxInR0,
                 BusMuxInR14,
                 BusMuxInR15,
 
-
                 BusMuxInHI,
-		BusMuxInLO,
+					 BusMuxInLO,
                 BusMuxInZHI,
-		BusMuxInZLO,
+					 BusMuxInZLO,
                 BusMuxInPc,
                 BusMuxInMDR,
                 BusMuxInInPort,
-                CSignExtended,
-
+                CSignExtended;
+input wire [4:0] 	 EncodeOut;
 
 output reg [31:0] BusMuxOut;
                         
 
-always @(posedge clk) 
+always begin
+	
+	case (EncodeOut)
+	5'b00001 : BusMuxOut = BusMuxInR0;
+	5'b00010 : BusMuxOut = BusMuxInR0;
+	5'b00100 : BusMuxOut = BusMuxInR0;
+	5'b01000 : BusMuxOut = BusMuxInR0;
+	5'b10000 : BusMuxOut = BusMuxInR0;
+	5'b00011 : BusMuxOut = BusMuxInR0;
+	5'b00101 : BusMuxOut = BusMuxInR0;
+	5'b01001 : BusMuxOut = BusMuxInR0;
+	5'b10001 : BusMuxOut = BusMuxInR0;
+	5'b00110 : BusMuxOut = BusMuxInR0;
+	5'b01010 : BusMuxOut = BusMuxInR0;
+	5'b10100 : BusMuxOut = BusMuxInR0;
+	5'b01100 : BusMuxOut = BusMuxInR0;
+	5'b11000 : BusMuxOut = BusMuxInR0;
+	5'b01101 : BusMuxOut = BusMuxInR0;
+	5'b10101 : BusMuxOut = BusMuxInR0;
+	5'b10110 : BusMuxOut = BusMuxInR0;
+	5'b01110 : BusMuxOut = BusMuxInR0;
+	5'b11010 : BusMuxOut = BusMuxInR0;
+	5'b11100 : BusMuxOut = BusMuxInR0;
+	5'b10111 : BusMuxOut = BusMuxInR0;
+	5'b11001 : BusMuxOut = BusMuxInR0;
+	5'b11011 : BusMuxOut = BusMuxInR0;
+	5'b11101 : BusMuxOut = BusMuxInR0;
+	
+endcase
+end
+endmodule
 //Might not be required. if using clk, put into module statement.
