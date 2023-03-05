de0c9f993f1b9476a7922123c71425ba89230f39
Silverseed44 <97930685+Silverseed44@users.noreply.github.com>
2023-03-05 15:50:16 -0500

    Update Multiplexer.v

--- Multiplexer.v
+++ Multiplexer.v
@@ -60,34 +60,34 @@ input wire [4:0] 	 EncodeOut;
 output reg [31:0] BusMuxOut;
                         
 
-always begin
+always @(*) begin
 	
 	case (EncodeOut)
-	5'b00001 : BusMuxOut = BusMuxInR0;
-	5'b00010 : BusMuxOut = BusMuxInR0;
-	5'b00100 : BusMuxOut = BusMuxInR0;
-	5'b01000 : BusMuxOut = BusMuxInR0;
-	5'b10000 : BusMuxOut = BusMuxInR0;
-	5'b00011 : BusMuxOut = BusMuxInR0;
-	5'b00101 : BusMuxOut = BusMuxInR0;
-	5'b01001 : BusMuxOut = BusMuxInR0;
-	5'b10001 : BusMuxOut = BusMuxInR0;
-	5'b00110 : BusMuxOut = BusMuxInR0;
-	5'b01010 : BusMuxOut = BusMuxInR0;
-	5'b10100 : BusMuxOut = BusMuxInR0;
-	5'b01100 : BusMuxOut = BusMuxInR0;
-	5'b11000 : BusMuxOut = BusMuxInR0;
-	5'b01101 : BusMuxOut = BusMuxInR0;
-	5'b10101 : BusMuxOut = BusMuxInR0;
-	5'b10110 : BusMuxOut = BusMuxInR0;
-	5'b01110 : BusMuxOut = BusMuxInR0;
-	5'b11010 : BusMuxOut = BusMuxInR0;
-	5'b11100 : BusMuxOut = BusMuxInR0;
-	5'b10111 : BusMuxOut = BusMuxInR0;
-	5'b11001 : BusMuxOut = BusMuxInR0;
-	5'b11011 : BusMuxOut = BusMuxInR0;
-	5'b11101 : BusMuxOut = BusMuxInR0;
-	
+	5'b00001 : BusMuxOut <= BusMuxInR0;
+	5'b00010 : BusMuxOut <= BusMuxInR1;
+	5'b00100 : BusMuxOut <= BusMuxInR2;
+	5'b01000 : BusMuxOut <= BusMuxInR3;
+	5'b10000 : BusMuxOut <= BusMuxInR4;
+	5'b00011 : BusMuxOut <= BusMuxInR5;
+	5'b00101 : BusMuxOut <= BusMuxInR6;
+	5'b01001 : BusMuxOut <= BusMuxInR7;
+	5'b10001 : BusMuxOut <= BusMuxInR8;
+	5'b00110 : BusMuxOut <= BusMuxInR9;
+	5'b01010 : BusMuxOut <= BusMuxInR10;
+	5'b10100 : BusMuxOut <= BusMuxInR1;
+	5'b01100 : BusMuxOut <= BusMuxInR12;
+	5'b11000 : BusMuxOut <= BusMuxInR13;
+	5'b01101 : BusMuxOut <= BusMuxInR14;
+	5'b10101 : BusMuxOut <= BusMuxInR15;
+	5'b10110 : BusMuxOut <= BusMuxInHI;
+	5'b01110 : BusMuxOut <= BusMuxInLO;
+	5'b11010 : BusMuxOut <= BusMuxInZHI;
+	5'b11100 : BusMuxOut <= BusMuxInZLO;
+	5'b10111 : BusMuxOut <= BusMuxInPc;
+	5'b11001 : BusMuxOut <= BusMuxInMDR;
+	5'b11011 : BusMuxOut <= BusMuxInInPort;
+	5'b11101 : BusMuxOut <= CSignExtended;
+	default : BusMuxOut <= 0;
 endcase
 end
 endmodule
