c800e258aa808d0c16563f4ee0cdc0290539ee3b
Josh Argiropoulos <122761374+JoshuaArgiropoulos@users.noreply.github.com>
2023-03-06 17:32:16 -0500

    Update Encoder.v

--- Mar6/Encoder.v
+++ Mar6/Encoder.v
@@ -33,32 +33,4 @@ module Encoder(EncodeIn, EncodeOut);
 	 endcase
 	end
 	endmodule
-	 /*
-	 if (00000000000000000000000000000000 <= EncodeIn) assign EncodeOut = 5'b00000;
-    if (00000000000000000000000000000001 <= EncodeIn) assign EncodeOut = 5'b00001;
-    if (00000000000000000000000000000010 <= EncodeIn) assign EncodeOut = 5'b00010;
-    if (00000000000000000000000000000100 <= EncodeIn) assign EncodeOut = 5'b00100;
-    if (00000000000000000000000000001000 <= EncodeIn) assign EncodeOut = 5'b01000;
-    if (00000000000000000000000000010000 <= EncodeIn) assign EncodeOut = 5'b10000;
-    if (00000000000000000000000000100000 <= EncodeIn) assign EncodeOut = 5'b00011;
-    if (00000000000000000000000001000000 <= EncodeIn) assign EncodeOut = 5'b00101;
-    if (00000000000000000000000010000000 <= EncodeIn) assign EncodeOut = 5'b01001;
-    if (00000000000000000000000100000000 <= EncodeIn) assign EncodeOut = 5'b10001;
-    if (00000000000000000000001000000000 <= EncodeIn) assign EncodeOut = 5'b00110;
-    if (00000000000000000000010000000000 <= EncodeIn) assign EncodeOut = 5'b01010;
-    if (00000000000000000000100000000000 <= EncodeIn) assign EncodeOut = 5'b10100;
-    if (00000000000000000001000000000000 <= EncodeIn) assign EncodeOut = 5'b01100;
-    if (00000000000000000010000000000000 <= EncodeIn) assign EncodeOut = 5'b11000;
-    if (00000000000000000100000000000000 <= EncodeIn) assign EncodeOut = 5'b01101;
-    if (00000000000000001000000000000000 <= EncodeIn) assign EncodeOut = 5'b10101;
-    if (00000000000000010000000000000000 <= EncodeIn) assign EncodeOut = 5'b10110;
-    if (00000000000000100000000000000000 <= EncodeIn) assign EncodeOut = 5'b01110;
-    if (00000000000001000000000000000000 <= EncodeIn) assign EncodeOut = 5'b11010;
-    if (00000000000010000000000000000000 <= EncodeIn) assign EncodeOut = 5'b11100;
-    if (00000000000100000000000000000000 <= EncodeIn) assign EncodeOut = 5'b10111;
-    if (00000000001000000000000000000000 <= EncodeIn) assign EncodeOut = 5'b11001;
-    if (00000000010000000000000000000000 <= EncodeIn) assign EncodeOut = 5'b11011;
-    if (00000000100000000000000000000000 <= EncodeIn) assign EncodeOut = 5'b11101;
-	 */
-	
-
+	 
