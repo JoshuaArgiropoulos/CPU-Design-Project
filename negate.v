bdbac726541e5111aa34b820aea1c94f0f9c8a1e
Liam O'Regan <20lqor@queensu.ca>
2023-03-10 16:55:20 -0500

    moved old files

--- negate.v
+++
@@ -1,8 +0,0 @@
-module negate(value, result);
-
-input [31:0] value;
-output [31:0] result;
-
-assign result = -value;
-
-endmodule
--- notOp.v
+++
@@ -1,8 +0,0 @@
-module notOp(value, result);
-
-input [31:0] value;
-output [31:0] result;
-
-assign result = -value - 1;
-
-endmodule
